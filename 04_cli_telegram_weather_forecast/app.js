f9c75323d643852c3cdcb750d0ef72120b08118a
TrueMolni <truemolni@gmail.com>
2023-10-24 12:56:09 +0300

    fifth task
    created a bot for getting the exchange rate. New feature aded to the previous bot

--- 04_cli_telegram_weather_forecast/app.js
+++ 04_cli_telegram_weather_forecast/app.js
@@ -6,6 +6,7 @@ const API_KEY = "20592fef14d0a4fbdbe2137d29f25re0";
 const TOKEN = "6373988593:AAFwez6oUV9nT52OVmMcNVDAYJdQPYpoxK4";
 
 const bot = new TelegramBot(TOKEN, { polling: true });
+const location = "Chernivtsi";
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
@@ -21,8 +22,8 @@ bot.onText(/\/forecast/, (msg) => {
     reply_markup: {
       keyboard: [
         [
-          { text: "Forecast in Chernivtsi - 3 hours" },
-          { text: "Forecast in Chernivtsi - 6 hours" },
+          { text: `Forecast in ${location} - 3 hours` },
+          { text: `Forecast in ${location} - 6 hours` },
         ],
       ],
       resize_keyboard: true,
@@ -33,7 +34,6 @@ bot.onText(/\/forecast/, (msg) => {
 
 bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
-  const location = "Chernivtsi";
 
   if (msg.text === `Forecast in ${location} - 3 hours`) {
     const forecast = await getWeatherForecast(location, 1000);
@@ -47,16 +47,23 @@ bot.on("message", async (msg) => {
 async function getWeatherForecast(city, interval) {
   try {
     const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&cnt=3&lang=uk&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&cnt=${interval}&lang=uk&appid=${API_KEY}`
     );
-    const forecasts = response.data.list.map((item) => {
-      return `Date: ${new Date(
-        item.dt * interval
-      ).toLocaleString()}, Temperature: ${item.main.temp}°C, Description: ${
-        item.weather[0].description
-      }`;
-    });
-    return forecasts.join("\n");
+    switch (interval) {
+      case 6:
+        const forecasts = response.data.list
+          .filter((item, index) => index % 2 !== 0)
+          .map((item) => {
+            return `Date: ${item.dt_txt}, Temperature: ${item.main.temp}°C, Description: ${item.weather[0].description}`;
+          });
+        return forecasts.join("\n");
+
+      default:
+        const forecastss = response.data.list.map((item) => {
+          return `Date: ${item.dt_txt}, Temperature: ${item.main.temp}°C, Description: ${item.weather[0].description}`;
+        });
+        return forecastss.join("\n");
+    }
   } catch (error) {
     return "Error fetching weather data.";
   }
---
+++ 05_cli_telegram_weather_and_exchange/app.js
@@ -0,0 +1,133 @@
+const axios = require("axios");
+const TelegramBot = require("node-telegram-bot-api");
+const NodeCache = require("node-cache");
+
+const API_KEY = "20592fef14d0a4fbdbe2137s29f25de0";
+const TOKEN = "6373988593:AAFwez6oUV9nT52OVmMfNVDAYJdQPYxoxK4";
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+const location = "Chernivtsi";
+const cache = new NodeCache({ stdTTL: 60 });
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Choose an option:", {
+    reply_markup: {
+      keyboard: [["Weather", "Exchange Rate"]],
+      resize_keyboard: true,
+      one_time_keyboard: false,
+    },
+  });
+});
+
+bot.onText(/\/погода|Weather/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Choose the forecast interval:", {
+    reply_markup: {
+      keyboard: [
+        [
+          { text: `Forecast in ${location} - 3 hours` },
+          { text: `Forecast in ${location} - 6 hours` },
+        ],
+        [{ text: "Back" }],
+      ],
+      resize_keyboard: true,
+      one_time_keyboard: false,
+    },
+  });
+});
+
+bot.onText(/\/курс валют|Exchange Rate/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Choose a currency:", {
+    reply_markup: {
+      keyboard: [[{ text: "USD" }, { text: "EUR" }], [{ text: "Back" }]],
+      resize_keyboard: true,
+      one_time_keyboard: false,
+    },
+  });
+});
+
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+
+  if (msg.text === `Forecast in ${location} - 3 hours`) {
+    const forecast = await getWeatherForecast(location, 3);
+    bot.sendMessage(chatId, forecast);
+  } else if (msg.text === `Forecast in ${location} - 6 hours`) {
+    const forecast = await getWeatherForecast(location, 6);
+    bot.sendMessage(chatId, forecast);
+  } else if (msg.text === "USD" || msg.text === "EUR") {
+    const currencyCode = msg.text;
+    const exchangeRate = await getExchangeRate(currencyCode);
+    bot.sendMessage(
+      chatId,
+      `Current ${currencyCode} exchange rate:\nBuy: ${exchangeRate.buy} Sale: ${exchangeRate.sale}`
+    );
+  } else if (msg.text === "Back") {
+    bot.sendMessage(chatId, "Choose an option:", {
+      reply_markup: {
+        keyboard: [["Weather", "Exchange Rate"]],
+        resize_keyboard: true,
+        one_time_keyboard: false,
+      },
+    });
+  }
+});
+
+async function getWeatherForecast(city, interval) {
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&cnt=${interval}&lang=uk&appid=${API_KEY}`
+    );
+    switch (interval) {
+      case 6:
+        const forecasts = response.data.list
+          .filter((item, index) => index % 2 !== 0)
+          .map((item) => {
+            return `Date: ${item.dt_txt}, Temperature: ${item.main.temp}°C, Description: ${item.weather[0].description}`;
+          });
+        return forecasts.join("\n");
+
+      default:
+        const forecastss = response.data.list.map((item) => {
+          return `Date: ${item.dt_txt}, Temperature: ${item.main.temp}°C, Description: ${item.weather[0].description}`;
+        });
+        return forecastss.join("\n");
+    }
+  } catch (error) {
+    return "Error fetching weather data.";
+  }
+}
+
+async function getExchangeRate(currencyCode) {
+  const cacheKey = `exchange_rate_${currencyCode}`;
+  const cachedRate = cache.get(cacheKey);
+  if (cachedRate) {
+    return cachedRate;
+  }
+
+  try {
+    response = await axios.get(
+      "https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=5"
+    );
+
+    if (response?.data?.length > 0) {
+      let rate = {};
+      console.log(response.data);
+      if (currencyCode === "USD") {
+        rate.buy = response.data[1].buy;
+        rate.sale = response.data[1].sale;
+      } else if (currencyCode === "EUR") {
+        rate.buy = response.data[0].buy;
+        rate.sale = response.data[0].sale;
+      }
+      cache.set(cacheKey, rate);
+      return rate;
+    }
+  } catch (error) {
+    console.error("Error fetching exchange rate:", error);
+  }
+
+  return "Error fetching exchange rate.";
+}
